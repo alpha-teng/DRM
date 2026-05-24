f5c0e944c882e6236aa8f1ce74b91de5b33fedc6
alphateng <13003311398@163.com>
2026-05-24 12:11:40 +0800

    feat: 系统名称从'园区招商'改为'医院决策管理系统'

--- ai58-ui/src/views/login.vue
+++ ai58-ui/src/views/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="login">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">园区招商管理系统</h3>
+      <h3 class="title">医院决策管理系统</h3>
       <el-form-item prop="username">
         <el-input
           v-model="loginForm.username"
